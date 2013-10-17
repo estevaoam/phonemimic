168b0f4872b94114c448d5e0b04f6cdfeb64861c
Estevão Mascarenhas <estevao.am@gmail.com>
2013-10-17 10:39:56 -0300

    Trying to sync the cube movement with the smartphone

--- public/javascripts/server.js
+++ public/javascripts/server.js
@@ -11,8 +11,47 @@ $(function(){
   });
 
   socket.on('showMovements', function(data){
-    xEl.html(data.beta);
-    yEl.html(data.gamma);
-    zEl.html(data.alpha);
+    var x = data.beta.toFixed(2),
+        y = data.gamma.toFixed(2),
+        z = data.alpha.toFixed(2);
+
+    if (firstTime) {
+      firstTime = false;
+      init();
+    }
+
+    animate(x, y, z);
   });
+
+
+  var camera, scene, renderer;
+  var geometry, material, mesh;
+
+  function init() {
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.CubeGeometry( 200, 200, 200 );
+    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    renderer = new THREE.CanvasRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    $('body').html( renderer.domElement );
+  }
+
+  function animate(x, y, z) {
+
+    mesh.rotation.x = x;
+    mesh.rotation.y = y;
+    mesh.rotation.z = z;
+
+    renderer.render( scene, camera );
+  }
 });
