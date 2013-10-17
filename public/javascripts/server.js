d8aacda9c9925435e312f3feb48a3d30747b5b4f
Estevão Mascarenhas <estevao.am@gmail.com>
2013-10-17 18:29:57 -0300

    Fixing movements

--- public/javascripts/server.js
+++ public/javascripts/server.js
@@ -2,6 +2,12 @@ $(function(){
   var host = location.origin.replace(/^http/, 'ws');
   var socket = io.connect(host + '/' + sid);
   var firstTime = true;
+  var camera, scene, renderer;
+  var geometry, material, mesh;
+  var mouseX = 0, mouseY = 0;
+  var windowHalfX = window.innerWidth / 2;
+  var windowHalfY = window.innerHeight / 2;
+
 
   var xEl = $('#x'),
       yEl = $('#y'),
@@ -19,41 +25,65 @@ $(function(){
     if (firstTime == true) {
       firstTime = false;
       init();
+      animate();
     }
 
-    animate(x, y, z);
+    mesh.rotation.x = x*Math.PI / 180;
+    mesh.rotation.y = z*Math.PI / 180;
+    mesh.rotation.z = -1 * (y*Math.PI / 180);
   });
-
-
-  var camera, scene, renderer;
-  var geometry, material, mesh;
 
   function init() {
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.z = 500;
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.CubeGeometry( 58.6 * 5, 7.6 * 5, 123.8 * 5 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    var ambient = new THREE.AmbientLight( 0x101030 );
+    scene.add( ambient );
 
-    renderer = new THREE.CanvasRenderer();
+    var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+    directionalLight.position.set( 0, 0, 1 ).normalize();
+    scene.add( directionalLight );
+
+    var loader = new THREE.OBJMTLLoader();
+    loader.load('/iPhone5.obj', '/iPhone5.mtl', function(object){
+      mesh = object;
+      mesh.position.y = -80;
+      scene.add(mesh);
+    });
+
+    renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
     $('body').html( renderer.domElement );
   }
 
-  function animate(x, y, z) {
+  function animate() {
+    requestAnimationFrame(animate);
+    render();
+  }
 
-    mesh.rotation.x = x*Math.PI / 180;
-    mesh.rotation.y = z*Math.PI / 180;
-    mesh.rotation.z = -1 * (y*Math.PI / 180);
+  function render() {
+    console.log(mouseX, mouseY);
+    camera.position.x += ( mouseX - camera.position.x ) * .05;
+    camera.position.y += ( - mouseY - camera.position.y ) * .05;
+    camera.lookAt( scene.position );
 
     renderer.render( scene, camera );
   }
 
+
+  function onDocumentMouseMove( event ) {
+    mouseX = ( event.clientX - windowHalfX ) / 2;
+    mouseY = ( event.clientY - windowHalfY ) / 2;
+  }
+
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+  init();
+  animate();
 });
+
